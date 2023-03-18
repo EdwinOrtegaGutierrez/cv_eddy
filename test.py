1d3eb7b69be4baec8ab1cd7ac06345d5b7abe992
EdwinOrtegaGutierrez <eddyded49@gmail.com>
2023-03-18 00:41:28 -0600

    It was implemented an improvement in skills, and a better startup in .sh

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from json import load
+from requests import get
+from json import loads
+# La URL de la API para obtener la información de los repositorios de un usuario
+url = 'https://api.github.com/users/EdwinOrtegaGutierrez/repos'
 
-with open('static/json/skills.json') as f:
-    data = load(f)
-skills = data["skills"]
-db = data["db"]
-pl = data["pl"]
-frameworks = data["frameworks"]
-print(frameworks)
+# Crea una solicitud GET a la API con el token de acceso en la cabecera
+response = get(url, headers={'Authorization': 'token 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'})
+
+# Obtiene la lista de repositorios de la respuesta JSON utilizando la biblioteca "json"
+repos = loads(response.content)
+
+# Crea el diccionario de repositorios utilizando una comprensión de diccionario
+repositories = {repo['name']: {'description': repo['description'], 'url': repo['html_url']} for repo in repos}
+
+print(repositories)
